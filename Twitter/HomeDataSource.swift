6d118613a679e58b596de877d52c450cd117edde
Xiaoheng Pan <xiaoheng.pan@tinroofsoftware.com>
2017-04-16 22:23:00 -0400

    update dynamic cell

--- Twitter/HomeDataSource.swift
+++ Twitter/HomeDataSource.swift
@@ -13,11 +13,10 @@ class HomeDataSource: Datasource {
     let users: [User] = {
         let brianUser = User(name: "Brian", username: "@letsbuildthatapp", bioText: "Some more bio text", profileImage: #imageLiteral(resourceName: "profile"))
         let rayUser = User(name: "Ray", username: "@rwenderlich", bioText: "Ray Wenderlich is an iPhone developer and tweets on topics related to iPhone, software, and gaming. Check out our conference.", profileImage: #imageLiteral(resourceName: "profile2"))
+        let kindleCoursePromotion = User(name: "Kindle Course", username: "@kindlecourse", bioText: "This is a long message trying to figure out if the view will display correctly when the biotext is really long. I thoroughly enjoylasjdf;laijsd;ijfa;lks ijf;kdifj aksf;ijas;df lajsdli ja;sdjf ;lasjf ;lasjdf;ija s;dlkjf;a lsdjfli jas;ldfj ;aldsjf idjjasd;lkfja  a;lkdjf;lasjdfjasjf  ;alkdfj ;aslkdjfj faksdj faldjf asdfj aksdfj a;sldkfj akds ksjfkajd", profileImage: #imageLiteral(resourceName: "profile"))
         
-        return [brianUser, rayUser]
+        return [brianUser, rayUser, kindleCoursePromotion]
     }()
-    
-//    let words = ["user1", "user2", "user3"]
     
     override func item(_ indexPath: IndexPath) -> Any? {
         return users[indexPath.item]
--- Twitter/HomeDataSourceController.swift
+++ Twitter/HomeDataSourceController.swift
@@ -18,7 +18,20 @@ class HomeDataSourceController: DatasourceController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        
+        if let user = self.datasource?.item(indexPath) as? User {
+
+            // get estimation for height of cell based off bioText content.
+            let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
+            let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
+            let attribute = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attribute, context: nil)
+            
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+
+        
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
@@ -27,6 +40,10 @@ class HomeDataSourceController: DatasourceController {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
 }
